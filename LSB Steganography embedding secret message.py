26a924378b9f43d9a697be14ec0e6c073bad4b4f
Justinocholi <74488931+Justinocholi@users.noreply.github.com>
2024-08-28 01:52:48 +0100

    Add files via upload

---
+++ LSB Steganography embedding secret message.py
@@ -0,0 +1,47 @@
+# IMPORT LIBRARIES
+
+import numpy as np
+import PIL.Image
+
+# SELECT HIDDEN MESSAGE
+ 
+message_to_hide = "Samuel Okpe"
+
+# LOAD IMAGE
+image = PIL.Image.open('image.png', 'r')
+width, height = image.size
+img_arr = np.array(list(image.getdata()))
+
+if image.mode == "P":
+    print("Not supported")
+    exit()
+
+channels = 4 if image.mode == "RGBA" else 3
+
+pixels = img_arr.size // channels
+
+# STOP INDICATOR OR KEY
+stop_indicator = "$CYBERSECURITY$"
+stop_indicator_length = len(stop_indicator)
+
+message_to_hide += stop_indicator
+
+
+byte_message = ''.join(f"{ord(c): 08b}" for c in message_to_hide)
+bits = len(byte_message)
+
+
+if bits > pixels:
+    print("Not enough space")
+else:
+    index = 0
+    for i in range(pixels):
+        for j in range(0,3):
+            if index < bits:
+                img_arr[i][j] = int(bin(img_arr[i][j])[2:] + byte_message[index], 2)
+                index += 1
+img_arr = img_arr.reshape((height, width, channels))
+result = PIL.Image.fromarray(img_arr.astype('uint8'), image.mode)
+result.save('encoded.png')
+
+# SAVES THE MESSAGE IN THE IMAGE
